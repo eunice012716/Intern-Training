1114e0ebd6c210fb0e085bd7dc00905e56107972
txya900619 <wayne900619@gmail.com>
2021-07-18 18:14:38 +0000

    Modify 2.2 exercises variable name to underscore and change function comment type

--- txya900619/Week1/ch2/2.2/exercise1.py
+++ txya900619/Week1/ch2/2.2/exercise1.py
@@ -2,27 +2,27 @@ import pandas as pd
 import numpy as np
 
 
-def get_random_na_dataframe(dataFrame: pd.DataFrame) -> pd.DataFrame:
-    # random insert na to dataframe
+def get_random_na_dataframe(dataframe: pd.DataFrame) -> pd.DataFrame:
+    """random insert na to dataframe"""
     na_probability = 0.2
-    return dataFrame.mask(np.random.random(dataFrame.shape) < na_probability)
+    return dataframe.mask(np.random.random(dataframe.shape) < na_probability)
 
 
-def process_data(dataFrame: pd.DataFrame) -> pd.DataFrame:
-    # delete column that have most na, and fill na with mean
+def process_data(dataframe: pd.DataFrame) -> pd.DataFrame:
+    """delete column that have most na, and fill na with mean"""
 
-    naCount = dataFrame.isnull().sum(axis=0).tolist()
-    mostNaCol = naCount.index(max(naCount))
-    result = dataFrame.drop(dataFrame.columns[mostNaCol], axis=1)
+    na_count = dataframe.isnull().sum(axis=0).tolist()
+    most_na_col = na_count.index(max(na_count))
+    result = dataframe.drop(dataframe.columns[most_na_col], axis=1)
     return result.fillna(result.mean())
 
 
 if __name__ == "__main__":
     with open("example_dataset.csv", "r", encoding="utf-8") as example_dataset:
-        dataFrame = pd.read_csv(example_dataset)
+        dataframe = pd.read_csv(example_dataset)
         inputs, outputs = (
-            get_random_na_dataframe(dataFrame.iloc[:, :-1]),
-            dataFrame.iloc[:, -1],
+            get_random_na_dataframe(dataframe.iloc[:, :-1]),
+            dataframe.iloc[:, -1],
         )
         print("raw inputs:\n", inputs.head())
         print()
--- txya900619/Week1/ch2/2.2/exercise2.py
+++ txya900619/Week1/ch2/2.2/exercise2.py
@@ -4,11 +4,11 @@ import torch
 from exercise1 import get_random_na_dataframe, process_data
 
 if __name__ == "__main__":
-    with open("example_dataset.csv", "r") as f:
-        dataFrame = pd.read_csv(f)
+    with open("example_dataset.csv", "r", encoding="utf-8") as example_dataset:
+        dataframe = pd.read_csv(example_dataset)
         inputs, outputs = (
-            process_data(get_random_na_dataframe(dataFrame.iloc[:, :-1])),
-            dataFrame.iloc[:, -1],
+            process_data(get_random_na_dataframe(dataframe.iloc[:, :-1])),
+            dataframe.iloc[:, -1],
         )
 
         X, y = torch.tensor(inputs.values), torch.tensor(outputs.values)
