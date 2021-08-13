2c1fc7fe2606fb22d2355e7f7ad3df66dfdddcb5
txya900619 <wayne900619@gmail.com>
2021-08-13 16:34:33 +0800

    Add all ch10 example to txya900619 folder

---
+++ txya900619/Week4/ch10/10.5/example.py
@@ -0,0 +1,82 @@
+import torch
+from torch import nn
+from d2l import torch as d2l
+
+
+def transpose_qkv(X, num_heads):
+    # Shape of input `X`:
+    # (`batch_size`, no. of queries or key-value pairs, `num_hiddens`).
+    # Shape of output `X`:
+    # (`batch_size`, no. of queries or key-value pairs, `num_heads`,
+    # `num_hiddens` / `num_heads`)
+    X = X.reshape(X.shape[0], X.shape[1], num_heads, -1)
+
+    # Shape of output `X`:
+    # (`batch_size`, `num_heads`, no. of queries or key-value pairs,
+    # `num_hiddens` / `num_heads`)
+    X = X.permute(0, 2, 1, 3)
+
+    # Shape of `output`:
+    # (`batch_size` * `num_heads`, no. of queries or key-value pairs,
+    # `num_hiddens` / `num_heads`)
+    return X.reshape(-1, X.shape[2], X.shape[3])
+
+
+def transpose_output(X, num_heads):
+    X = X.reshape(-1, num_heads, X.shape[1], X.shape[2])
+    X = X.permute(0, 2, 1, 3)
+    return X.reshape(X.shape[0], X.shape[1], -1)
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(
+        self,
+        key_size,
+        query_size,
+        value_size,
+        num_hiddens,
+        num_heads,
+        dropout,
+        bias=False,
+        **kwargs,
+    ):
+        super(MultiHeadAttention, self).__init__(**kwargs)
+        self.num_heads = num_heads
+        self.attention = d2l.DotProductAttention(dropout)
+        self.W_q = nn.Linear(query_size, num_hiddens, bias=bias)
+        self.W_k = nn.Linear(key_size, num_hiddens, bias=bias)
+        self.W_v = nn.Linear(value_size, num_hiddens, bias=bias)
+        self.W_o = nn.Linear(num_hiddens, num_hiddens, bias=bias)
+
+    def forward(self, queries, keys, values, valid_lens):
+        queries = transpose_qkv(self.W_q(queries), self.num_heads)
+        keys = transpose_qkv(self.W_k(keys), self.num_heads)
+        values = transpose_qkv(self.W_v(values), self.num_heads)
+
+        if valid_lens is not None:
+            valid_lens = torch.repeat_interleave(
+                valid_lens, repeats=self.num_heads, dim=0
+            )
+
+        output = self.attention(queries, keys, values, valid_lens)
+        output_concat = transpose_output(output, self.num_heads)
+        return self.W_o(output_concat)
+
+
+if __name__ == "__main__":
+    NUM_HIDDENS = 100
+    NUM_HEADS = 5
+    attention = MultiHeadAttention(
+        NUM_HIDDENS, NUM_HIDDENS, NUM_HIDDENS, NUM_HIDDENS, NUM_HEADS, 0.5
+    )
+    print(attention.eval())
+
+    batch_size, num_queries, num_kvpairs, valid_lens = (
+        2,
+        4,
+        6,
+        torch.tensor([3, 2]),
+    )
+    X = torch.ones((batch_size, num_queries, NUM_HIDDENS))
+    Y = torch.ones((batch_size, num_kvpairs, NUM_HIDDENS))
+    print(attention(X, Y, Y, valid_lens).shape)
